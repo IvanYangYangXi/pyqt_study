8473651ea27aee65702a1ea4c00cf27db4dd207f
IVANPC <523166477@qq.com>
2019-02-12 22:32:33 +0800

    .

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+# @Author : Ivan-杨杨兮 (523166477@qq.com)
+# @Link   : www.cgartech.com
+# @Date   : 2/12/2019, 10:21:46 PM
+
+
+print('import packge "core"')
+
+from . import main # 从当前目录导入main
---
+++ core/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# main.py
+# @Author : Ivan-杨杨兮 (523166477@qq.com)
+# @Link   : www.cgartech.com
+# @Date   : 2/12/2019, 10:23:29 PM
+
+
+from PyQt5 import QtGui
